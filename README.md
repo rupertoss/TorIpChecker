f0984d653f6a8f619d471ce54d994e07c56583cf
rupertoss <rakoski.rupertoss@gmail.com>
2018-04-12 11:56:51 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Tor Ip Checker
---
+++ src/main/java/com/rupertoss/toripchecker/TorIpCheckerApplication.java
@@ -0,0 +1,12 @@
+package com.rupertoss.toripchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TorIpCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TorIpCheckerApplication.class, args);
+	}
+}
